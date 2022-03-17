a3886dd27c879c0bbe00502d2ff924b362a4084c
Victor Quiroz <vquiroz2018@alu.uct.cl>
2022-03-17 17:24:35 -0300

    Intento de uso de librería para leer el json.

--- algoritmo C++/prueba.cpp
+++ algoritmo C++/prueba.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
+#include <json/value.h>
+#include <fstream>
+
 using namespace std;
 
 int main(){
-    //datos=[]
-    int cromosoma[]={};
-    float fitnessPob[]={};
 
-    int cromosoma_nuevo[]={};
-    float fitnessPob_nuevo[]={};
+	ifstream data("ejemplo2.json", ifstream::binary);
+
+    int m=40;
+
+    int cromosoma[m][154]={};
+    float fitnessPob[m]={};
+
+    int cromosoma_nuevo[m][154]={};
+    float fitnessPob_nuevo[m]={};
 
     float fitnessEvol[]={};
 
@@ -16,11 +23,10 @@ int main(){
     int crossover12[]={};
     int crossover21[]={};
     //prueba funcionamiento arreglos
-    cromosoma[0]=1;
+    cromosoma[0][0]=1;
     fitnessPob[0]=0.1;
+    cromosoma_nuevo[0][0]=2;
     //memoria XD (Buscar como trabajar bien con la memoria)
-    cout << cromosoma[0];
-    cout << "\n";
-    cout << fitnessPob[0];
+    cout << data;
     return 0;
 }
