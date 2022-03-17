754f21c02102bd01116bc64dd5394bc38cd02b7c
Victor Quiroz <vquiroz2018@alu.uct.cl>
2022-03-16 21:50:19 -0300

    Primeras pruebas del funcionamiento de arreglos en C++.

---
+++ algoritmo C++/prueba.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //datos=[]
+    int cromosoma[]={};
+    float fitnessPob[]={};
+
+    int cromosoma_nuevo[]={};
+    float fitnessPob_nuevo[]={};
+
+    float fitnessEvol[]={};
+
+    int itera[]={};
+
+    int crossover12[]={};
+    int crossover21[]={};
+    //prueba funcionamiento arreglos
+    cromosoma[0]=1;
+    fitnessPob[0]=0.1;
+    //memoria XD (Buascar como trabajar bien con la memoria)
+    cout << cromosoma[0];
+    cout << "\n";
+    cout << fitnessPob[0];
+    return 0;
+}
