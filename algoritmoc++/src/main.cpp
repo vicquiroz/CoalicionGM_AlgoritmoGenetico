b7f74b61b19d6fa457ff32ac71e5e2049bae338f
Victor Quiroz <vquiroz2018@alu.uct.cl>
2022-04-01 18:32:39 -0300

    Pruebas sobre punteros.

--- algoritmoc++/src/main.cpp
+++ algoritmoc++/src/main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <cstdlib>
+#include <random>
+#include <string>
 
-void main() {
-	std::cout << "hola";
+//srand() para usar la semilla
+using namespace std;
+
+int m = 40;
+float pmutacion_threshold = 0.2;
+float pr = 0.1;
+int seed = 20;
+
+
+void main(int argc, char* argv[]) {
+	int quorum = 74;
+	if (argc > 1) {
+		m = stoi(argv[0]);
+		pmutacion_threshold = stod(argv[1]);
+		pr = stod(argv[2]);
+		seed = stoi(argv[3]);
+	}
+	int** matrix = (int**)malloc(2*sizeof(int*));
+	int* linea = new int[2];
+	matrix[0] = linea;
+	linea[0] = 0;
+	linea[1] = 1;
+
+	cout << matrix[0][0];
+	cout << matrix[0][1];
+
+	int* linea = new int[2];
+	matrix[1] = linea;
+	linea[0] = 3;
+	cout << matrix[1][0];
 }
