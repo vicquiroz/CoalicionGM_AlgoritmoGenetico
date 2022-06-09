03eb297bed5d2e36ac97d84ccca0d05a0dde5684
DAngelc3 <elobos2018@alu.uct.cl>
2022-06-09 13:56:14 -0400

    feat: :sparkles: se agrega al script de bash la funcionalidad en linux

--- algoritmoc++/script.bash
+++ algoritmoc++/script.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 m=(20 30 40 50 60)
 pTresh=(0.1 0.15 0.2 0.25 0.3)
 pr=(0.01 0.05 0.1 0.15 0.2)
@@ -6,7 +7,10 @@ vot=votacion22105
 cd out/
 
 mkdir -p "Resultados/$vot"
+#para windows
 cd build/x64-release
+#para linux
+#cd build/linux-debug/
 let seed=1234
 let iter=1
 for repeat in {1..5}
@@ -22,7 +26,10 @@ do
                 mkdir -p "../../Resultados/$vot/m_$m_val/pTresh_$pTresh_val/pr_$pr_val"
                 echo "---------######## $iter/625 ######----------"
                 ((seed+=$repeat*2))
+                #para windows
                 ./algortimoc++.exe $m_val $pTresh_val $pr_val $seed || echo "---------######## error en:  m = $m_val, pTresh = $pTresh_val, pr = $pr_val, seed= $seed donde $repeat de 5 ######----------"
+                #para linux
+                #./algortimoc++ $m_val $pTresh_val $pr_val $seed || cat "$seed.txt" "../../Resultados/$vot/m_$m_val/pTresh_$pTresh_val/pr_$pr_val/histograma_m_${m_val}_pTresh_${pTresh_val}_pr_" "${seed}.txt" 
                 cp resultados.json "../../Resultados/$vot/m_$m_val/pTresh_$pTresh_val/pr_$pr_val/resultados_rep_${repeat}_m_${m_val}_pTresh_${pTresh_val}_pr_${pr_val}.json" 
                 cp hist.json "../../Resultados/$vot/m_$m_val/pTresh_$pTresh_val/pr_$pr_val/histograma_rep_${repeat}_m_${m_val}_pTresh_${pTresh_val}_pr_${pr_val}.json" 
                 rm resultados.json
