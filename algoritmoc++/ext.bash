578b09a1fd7ee1041d761bd8db3cad2f63bcdfb6
Victor Quiroz <vquiroz2018@alu.uct.cl>
2022-06-30 13:21:41 -0400

    Arreglo de archivo de extracción, y gráficos mejorados.

--- algoritmoc++/ext.bash
+++ algoritmoc++/ext.bash
@@ -5,13 +5,13 @@ source ~/.bashrc
 
 m=(20 30 40 50 60)
 pTresh=(0.1 0.15 0.2 0.25 0.3)
-pr=(0.01 0.05 0.1 0.15 0.2)
+pr=(0.1 0.11 0.125 0.14 0.15)
 
 vot=votacion22105
 
 let iter=1
 cd "./out/Resultados"
-for repeat in {1..5}
+for repeat in {1..50}
 do 
     for m_val in "${m[@]}"
     do
@@ -19,14 +19,15 @@ do
         do
             for pr_val in "${pr[@]}"
             do
-                echo "---------######## $iter/625 ######----------"
-                m=$(jq ".m" "./$vot/m_$m_val/pTresh_$pTresh_val/pr_$pr_val/resultados_rep_${repeat}_m_${m_val}_pTresh_${pTresh_val}_pr_${pr_val}.json") 
+                echo "---------######## $iter/6250 ######----------"
+                mdos=$(jq ".m" "./$vot/m_$m_val/pTresh_$pTresh_val/pr_$pr_val/resultados_rep_${repeat}_m_${m_val}_pTresh_${pTresh_val}_pr_${pr_val}.json") 
                 pt=$(jq ".pmutacion_threshold" "./$vot/m_$m_val/pTresh_$pTresh_val/pr_$pr_val/resultados_rep_${repeat}_m_${m_val}_pTresh_${pTresh_val}_pr_${pr_val}.json") 
                 p=$(jq ".pr" "./$vot/m_$m_val/pTresh_$pTresh_val/pr_$pr_val/resultados_rep_${repeat}_m_${m_val}_pTresh_${pTresh_val}_pr_${pr_val}.json") 
                 seed=$(jq ".seed" "./$vot/m_$m_val/pTresh_$pTresh_val/pr_$pr_val/resultados_rep_${repeat}_m_${m_val}_pTresh_${pTresh_val}_pr_${pr_val}.json") 
                 ite=$(jq ".numero_de_iteraciones" "./$vot/m_$m_val/pTresh_$pTresh_val/pr_$pr_val/resultados_rep_${repeat}_m_${m_val}_pTresh_${pTresh_val}_pr_${pr_val}.json") 
                 fit=$(jq ".fitness" "./$vot/m_$m_val/pTresh_$pTresh_val/pr_$pr_val/resultados_rep_${repeat}_m_${m_val}_pTresh_${pTresh_val}_pr_${pr_val}.json")
-                echo "$m, $pt, $p, $seed, $ite, $fit" >> prueba.csv
+
+                echo "$mdos, $pt, $p, $seed, $ite, $fit" >> prueba.csv
                 #cp hist.json "../../Resultados/$vot/m_$m_val/pTresh_$pTresh_val/pr_$pr_val/histograma_rep_${repeat}_m_${m_val}_pTresh_${pTresh_val}_pr_${pr_val}.json" 
                 ((iter+=1))
             done
--- graficos/graficoFitness.R
+++ graficos/graficoFitness.R
@@ -1,54 +1,45 @@
 library(ggplot2)
-
-data <- read.csv(file = 'prueba.csv')
-datos=as.data.frame(data)
+library(vioplot)
+data<-read.csv(file = 'prueba.csv')
+datos<-as.data.frame(data)
 names(datos) = c("m","p_tresh","pr","seed","iteraciones","fitness")
-#print(datos)
-
-#usar graficos de violin
-#fitness
-print(
-  ggplot(data=datos)+geom_point(aes(x=1:nrow(datos),y=fitness))
-)
-
-print(
-  ggplot(data=datos,aes(fitness))+geom_histogram()
-)
-
-print(
-  ggplot(data=datos)+geom_point(aes(x=m,y=fitness))
-)
+# Graficos Violin Fitness vs Iteraciones
+#par(mfrow = c(1, 2))
+GrafViolin<-function(val2,title){
+  vioplot(datos$fitness~val2,col=2:length(levels(data$m)),xlab=title,ylab="Fitness")
+  stripchart(datos$fitness~val2,vertical=TRUE,method="jitter",pch=19,add=TRUE,col=3:8)
+  #vioplot(datos$iteraciones~val2,col=2:length(levels(data$m)),xlab="M",ylab="Iteraciones")
+  #stripchart(datos$iteraciones~val2,vertical=TRUE,method="jitter",pch=19,add=TRUE,col=3:8)
+}
 
 
-print(
-  ggplot(data=datos)+geom_point(aes(x=p_tresh,y=fitness))
-)
+#vioplot(datos$fitness,horizontal=TRUE,xlab="Fitness",ylab="")
+#vioplot(datos$iteraciones,horizontal=TRUE,xlab="Iteraciones",ylab="")
+# Graficos M
+#GrafViolin(datos$m,"M")
+# Graficos P Tresh
+#GrafViolin(datos$p_tresh,"P_Tresh")
+# Graficos PR
+#GrafViolin(datos$pr,"Pr")
 
-print(
-  ggplot(data=datos)+geom_point(aes(x=pr,y=fitness))
-)
+TablaComb<-function(mval,ptreshval,prval){
+  datostemp<-datos[which(datos$m==mval & datos$p_tresh==ptreshval & datos$pr==prval),]
+  combtemp<-table(datostemp$fitness)/50
+  print(paste("Combinacion:  m=",as.character(mval),"  pTresh=",as.character(ptreshval)," pr=",as.character(prval)))
+  print(combtemp)
+  print("")
+}
+
+mV=c(20,30,40,50,60)
+pTreshV=c(0.1,0.15,0.2,0.25,0.3)
+prV=c(0.1,0.11,0.125,0.14,0.15)
+for(mValue in mV){
+  for(pTreshValue in pTreshV){
+    for(prValue in prV){
+      TablaComb(mValue,pTreshValue,prValue)
+    }
+  }
+}
 
 
-##iteraciones
 
-
-print(
-  ggplot(data=datos)+geom_point(aes(x=1:nrow(datos),y=iteraciones))
-)
-
-print(
-  ggplot(data=datos,aes(iteraciones))+geom_histogram()
-)
-
-print(
-  ggplot(data=datos)+geom_point(aes(x=m,y=iteraciones))
-)
-
-
-print(
-  ggplot(data=datos)+geom_point(aes(x=p_tresh,y=iteraciones))
-)
-
-print(
-  ggplot(data=datos)+geom_point(aes(x=pr,y=iteraciones))
-)
