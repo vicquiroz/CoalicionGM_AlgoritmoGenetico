64b83f838c5e4b4d7ba598d3f11cec4e8a7cdc36
aparra <aparra2018@alu.uct.cl>
2022-06-30 15:18:12 -0400

    Cambios a graficoFitness, reestructuracion de datos y graficos

--- graficos/graficoFitness.R
+++ graficos/graficoFitness.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(vioplot)
+library(corrplot)
 data<-read.csv(file = 'pruebaTresh04.csv')
 datos<-as.data.frame(data)
 names(datos) = c("m","p_tresh","pr","seed","iteraciones","fitness")
@@ -25,10 +26,12 @@ GrafViolin<-function(val2,title){
 TablaComb<-function(mval,ptreshval,prval){
   datostemp<-datos[which(datos$m==mval & datos$p_tresh==ptreshval & datos$pr==prval),]
   combtemp<-table(datostemp$fitness)/50
-  print(paste("Combinacion:  m=",as.character(mval),"  pTresh=",as.character(ptreshval)," pr=",as.character(prval)))
-  print(combtemp)
-  print("")
-  
+  if(combtemp[1]>0.85){
+    print(paste("Combinacion:  m=",as.character(mval),"  pTresh=",as.character(ptreshval)," pr=",as.character(prval)))
+    print(combtemp)
+    print("")
+    print(datostemp$seed)
+  }
 }
 
 mV=c(40)
@@ -42,5 +45,24 @@ for(mValue in mV){
   }
 }
 
+subdata1<-datos[which(datos$m==40 & datos$p_tresh==0.3 & datos$pr==0.14),]
+subdata2<-datos[which(datos$m==50 & datos$p_tresh==0.1 & datos$pr==0.15),]
 
+print("Calculo Varianza")
+print("Subdata M=40, PTresh=0.3, Pr=0.14")
+print(var(subdata1$fitness))
+print("")
+print("Subdata M=50, PTresh=0.1, Pr=0.15")
+print(var(subdata2$fitness))
+print("Varianza a mano")
+
+print("Calculo Desviacion Estandar")
+print("Subdata M=40, PTresh=0.3, Pr=0.14")
+pca<-prcomp(subdata1[6],scale=FALSE)     
+print(pca$sdev)
+print("")
+print("Subdata M=50, PTresh=0.1, Pr=0.15")
+pca<-prcomp(subdata2[6],scale=FALSE)     
+print(pca$sdev)
+print("")
 
