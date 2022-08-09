84e777f77a369df62407bb7737afa4affb52bf7d
aparra <aparra2018@alu.uct.cl>
2022-08-09 16:07:16 -0400

    Ejecuciones en Servidor Censei
    Se agrego un codigo que recopila las ejecuciones y las convierte a un archivo csv.
    Se agrego un codigo el cual permite graficar los csv de manera simplificada

---
+++ Server Censei/graficosIteraciones.R
@@ -0,0 +1,86 @@
+ library(ggplot2)
+library(vioplot)
+library(corrplot)
+data<-read.csv(file = 'valores_verif_ParamLimbo.csv',header = FALSE)
+
+m<-as.numeric(data$V1[1])
+pTresh<-as.numeric(data$V2[1])
+pr<-as.numeric(data$V3[1])
+
+datos<-as.data.frame(data)
+names(datos) = c("m","p_tresh","pr","seed","iteraciones","fitness")
+# Graficos Violin Fitness vs Iteraciones
+#par(mfrow = c(1, 2))
+GrafViolin<-function(val2,title){
+  vioplot(datos$fitness~val2,col=2:length(levels(data$m)),xlab=title,ylab="Fitness")
+  stripchart(datos$fitness~val2,vertical=TRUE,method="jitter",pch=19,add=TRUE,col=3:8)
+  vioplot(datos$iteraciones~val2,col=2:length(levels(data$m)),xlab="M",ylab="Iteraciones")
+  stripchart(datos$iteraciones~val2,vertical=TRUE,method="jitter",pch=19,add=TRUE,col=3:8)
+}
+
+
+vioplot(datos$fitness,horizontal=TRUE,xlab="Fitness",ylab="")
+vioplot(datos$iteraciones,horizontal=TRUE,xlab="Iteraciones",ylab="")
+# Graficos M
+#GrafViolin(datos$m,"M")
+# Graficos P Tresh
+#GrafViolin(datos$p_tresh,"P_Tresh")
+# Graficos PR
+#GrafViolin(datos$pr,"Pr")
+
+TablaCombFit<-function(mval,ptreshval,prval){
+  datostemp<-datos[which(datos$m==mval & datos$p_tresh==ptreshval & datos$pr==prval),]
+  combtemp<-table(datostemp$fitness)/100
+  #if(combtemp[1]>0.85){
+    print(paste("Combinacion:  m=",as.character(mval),"  pTresh=",as.character(ptreshval)," pr=",as.character(prval)))
+    print(combtemp)
+    print("")
+    #print(datostemp$seed)
+  #}
+}
+PromIter<-function(mval,ptreshval,prval){
+  # datostemp<-datos[which(datos$m==mval & datos$p_tresh==ptreshval & datos$pr==prval),]
+  iters<-mean(datostemp$iteraciones)
+  print(iters)
+}
+print("Para Fitness")
+mV=c(m)
+pTreshV=c(pTresh)
+prV=c(pr)
+for(mValue in mV){
+  for(pTreshValue in pTreshV){
+    for(prValue in prV){
+      TablaCombFit(mValue,pTreshValue,prValue)
+    }
+  }
+}
+
+subdata1<-datos[which(datos$m==m & datos$p_tresh==pTresh & datos$pr==pr),]
+
+print("Calculo Varianza")
+print(var(subdata1$fitness))
+
+print("Calculo Desviacion Estandar")
+pca<-prcomp(subdata1[6],scale=FALSE)     
+print(pca$sdev)
+print("")
+
+
+print("Para Iter")
+mV=c(m)
+pTreshV=c(pTresh)
+prV=c(pr)
+for(mValue in mV){
+  for(pTreshValue in pTreshV){
+    for(prValue in prV){
+      PromIter(mValue,pTreshValue,prValue)
+    }
+  }
+}
+print("Calculo Varianza")
+print(var(subdata1$iteraciones))
+
+print("Calculo Desviacion Estandar")
+pca<-prcomp(subdata1[5],scale=FALSE)     
+print(pca$sdev)
+print("")
---
+++ Server Censei/leer_verif.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+shopt -s expand_aliases
+source ~/.bashrc
+#alias jq=./jq-win64.exe
+
+vot=votacion22105
+let seed=1234
+let iter=1
+cd "ResultadosParamLimbo"
+for repeat in {1..100}
+do 
+    ((seed+=$repeat*2))
+    echo "---------######## $iter/100 ######----------"
+    m=$(jq ".m" "./$vot/resultados_rep_${repeat}_seed_${seed}.json") 
+    pt=$(jq ".pmutacion_threshold" "./$vot/resultados_rep_${repeat}_seed_${seed}.json") 
+    p=$(jq ".pr" "./$vot/resultados_rep_${repeat}_seed_${seed}.json") 
+    seed=$(jq ".seed" "./$vot/resultados_rep_${repeat}_seed_${seed}.json") 
+    ite=$(jq ".numero_de_iteraciones" "./$vot/resultados_rep_${repeat}_seed_${seed}.json") 
+    fit=$(jq ".fitness" "./$vot/resultados_rep_${repeat}_seed_${seed}.json")
+    echo "$m, $pt, $p, $seed, $ite, $fit" >> valores_verif.csv
+    #cp hist.json "../../Resultados/$vot/m_$m_val/pTresh_$pTresh_val/pr_$pr_val/histograma_rep_${repeat}_m_${m_val}_pTresh_${pTresh_val}_pr_${pr_val}.json" 
+    
+    ((iter+=1))
+done
