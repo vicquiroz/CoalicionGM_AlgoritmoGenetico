c6aebe0c4822e7351e06823de2f44faa7ba8a70e
ignaciolincolao <ilincolaovenegas@gmail.com>
2022-08-20 15:13:40 -0400

    add exp1,exp2,exp3,exp4 in limbo

---
+++ limbo/exp4/include/coalicionGM.hpp
@@ -0,0 +1,7 @@
+#ifndef MAIN_COALICIONGM_H
+#define MAIN_COALICIONGM_H
+#include <iostream>
+#include <tuple>
+std::tuple<float,int> coalicionGM(int m, float pmutacion_threshold, float pr);
+
+#endif
