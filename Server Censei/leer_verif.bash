8065816a0fe75946d61764d0223ec1f2ba331562
Victor Quiroz <vquiroz2018@alu.uct.cl>
2022-08-22 16:34:45 -0400

    verificación de 150

--- Server Censei/leer_verif.bash
+++ Server Censei/leer_verif.bash
@@ -7,17 +7,17 @@ vot=votacionRH0750158
 let seed=1234
 let iter=1
 cd "Resultados"
-for repeat in {1..1000}
+for repeat in {1..150}
 do 
     ((seed+=$repeat*2))
-    echo "---------######## $iter/1000 ######----------"
+    echo "---------######## $iter/150 ######----------"
     m=$(jq ".m" "./$vot/resultados_rep_${repeat}_seed_${seed}.json") 
     pt=$(jq ".pmutacion_threshold" "./$vot/resultados_rep_${repeat}_seed_${seed}.json") 
     p=$(jq ".pr" "./$vot/resultados_rep_${repeat}_seed_${seed}.json") 
     seed=$(jq ".seed" "./$vot/resultados_rep_${repeat}_seed_${seed}.json") 
     ite=$(jq ".numero_de_iteraciones" "./$vot/resultados_rep_${repeat}_seed_${seed}.json") 
     fit=$(jq ".fitness" "./$vot/resultados_rep_${repeat}_seed_${seed}.json")
-    echo "$m, $pt, $p, $seed, $ite, $fit" >> valores_verif_RH1170867.csv
+    echo "$m, $pt, $p, $seed, $ite, $fit" >> valores_verif_RH0750158.csv
     #cp hist.json "../../Resultados/$vot/m_$m_val/pTresh_$pTresh_val/pr_$pr_val/histograma_rep_${repeat}_m_${m_val}_pTresh_${pTresh_val}_pr_${pr_val}.json" 
     
     ((iter+=1))
