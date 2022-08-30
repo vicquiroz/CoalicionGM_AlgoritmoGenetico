6072b8767edfbb67b8c7dc9c3753ce108516e650
Victor Quiroz <vquiroz2018@alu.uct.cl>
2022-08-30 14:59:55 -0400

    grafico de soluciones.

--- graficos/graficoconvexUSA.r
+++ graficos/graficoconvexUSA.r
@@ -13,14 +13,20 @@ datosO<-data.frame(x=datosJson$x,y=datosJson$y,ID=c(1:lData))
 resultados<- fromJSON("resultados.json")
 resultadosDf<-data.frame(coalicion=resultados$coalicion)
 
-datosO$coaliciong<-c(1:lData)*0
+datosO$CGM<-c(1:lData)*0
 
 
-datosO$coaliciong[resultadosDf$coalicion+1]<-1
-datosO$coaliciong<-factor(datosO$coaliciong)
+datosO$CGM[resultadosDf$coalicion+1]<-1
+datosO$CGM<-factor(datosO$CGM)
 
 print(ggplot(data=datosO)+
-        geom_point(aes(x=x,y=y,col=coaliciong))+
-        geom_convexhull(data=datosO[which(datosO$coaliciong==1),1:2],aes(x=x,y=y),alpha = 0.2,col="cyan",fill="cyan"))
+        theme(aspect.ratio = 4/3)+
+        geom_point(aes(x=x,y=y)))
 
-sum(datosO$coaliciong=="0")
+print(ggplot(data=datosO)+
+        geom_point(aes(x=x,y=y,col=CGM))+
+        scale_color_manual(labels = c("No pertenece", "Pertenece"),
+                           values = c("red", "blue"))+
+        theme(aspect.ratio = 4/3)+
+        geom_convexhull(data=datosO[which(datosO$CGM==1),1:2],aes(x=x,y=y),alpha = 0.2,col="cyan",fill="cyan"))
+
