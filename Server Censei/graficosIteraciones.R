7ea72aa827fbd3b892cf02a1bed0672dea0d78de
Victor Quiroz <vquiroz2018@alu.uct.cl>
2022-08-31 13:38:54 -0400

    Grafico de resultados finales.

--- Server Censei/graficosIteraciones.R
+++ Server Censei/graficosIteraciones.R
@@ -1,7 +1,7 @@
  library(ggplot2)
 library(vioplot)
 library(corrplot)
-data<-read.csv(file = 'valores_verif_RH0750158-final-3000.csv',header = FALSE)
+data<-read.csv(file = 'valores_verif_RH0750158-final-10000.csv',header = FALSE)
 
 m<-as.numeric(data$V1[1])
 pTresh<-as.numeric(data$V2[1])
@@ -57,7 +57,7 @@ ggplot(datos, aes(x = fitness, y = iteraciones)) +
 
 TablaCombFit<-function(mval,ptreshval,prval){
   datostemp<-datos[which(datos$m==mval & datos$p_tresh==ptreshval & datos$pr==prval),]
-  combtemp<-table(datostemp$fitness)/3000
+  combtemp<-table(datostemp$fitness)/10000
   #if(combtemp[1]>0.85){
     print(paste("Combinacion:  m=",as.character(mval),"  pTresh=",as.character(ptreshval)," pr=",as.character(prval)))
     print(combtemp)
--- Server Censei/leer_verif.bash
+++ Server Censei/leer_verif.bash
@@ -7,17 +7,17 @@ vot=votacionRH0750158
 let seed=1234
 let iter=1
 cd "Resultados"
-for repeat in {1..3000}
+for repeat in {1..10000}
 do 
     ((seed+=$repeat*2))
-    echo "---------######## $iter/3000 ######----------"
+    echo "---------######## $iter/10000 ######----------"
     m=$(jq ".m" "./$vot/resultados_rep_${repeat}_seed_${seed}.json") 
     pt=$(jq ".pmutacion_threshold" "./$vot/resultados_rep_${repeat}_seed_${seed}.json") 
     p=$(jq ".pr" "./$vot/resultados_rep_${repeat}_seed_${seed}.json") 
     seed=$(jq ".seed" "./$vot/resultados_rep_${repeat}_seed_${seed}.json") 
     ite=$(jq ".numero_de_iteraciones" "./$vot/resultados_rep_${repeat}_seed_${seed}.json") 
     fit=$(jq ".fitness" "./$vot/resultados_rep_${repeat}_seed_${seed}.json")
-    echo "$m, $pt, $p, $seed, $ite, $fit" >> valores_verif_RH0750158-final-3000.csv
+    echo "$m, $pt, $p, $seed, $ite, $fit" >> valores_verif_RH0750158-final-10000.csv
     #cp hist.json "../../Resultados/$vot/m_$m_val/pTresh_$pTresh_val/pr_$pr_val/histograma_rep_${repeat}_m_${m_val}_pTresh_${pTresh_val}_pr_${pr_val}.json" 
     
     ((iter+=1))
