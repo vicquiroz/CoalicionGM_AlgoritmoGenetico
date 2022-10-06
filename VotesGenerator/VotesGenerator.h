7b155387588496269fc32e1d06c517832b35685e
aparra <aparra2018@alu.uct.cl>
2022-10-06 17:48:06 -0300

    Agregado generador de puntos aleatorios

---
+++ VotesGenerator/VotesGenerator.h
@@ -0,0 +1,6 @@
+﻿#include <iostream>
+double euclidian_distance(double x1, double y1, double x2, double y2)
+{
+	double calculation = pow(pow((x2 - x1), 2) + pow((y2 - y1), 2), 1 / (double)2);
+	return calculation;
+}
